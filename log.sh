3cf04adee7330c3663be5f588370cfb0c3b58f7e
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 15:12:03 -0400

    saving

--- log.sh
+++ log.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ev
 
-STR="Hello World!"
-echo $STR
-
 LG=$(git log --oneline $TRAVIS_COMMIT_RANGE)
-echo $LG
 LINK="<a href=\"https://github.com/zcompgeek/travis-broken-example/compare/$TRAVIS_COMMIT_RANGE\"> Full Commit Log</a>"
 echo $LINK
+
+echo "123dse3" > ./cache/salixHash
+
+OLD=$(cat ./cache/salixHash)
+echo $OLD
 
 phpunit Test.php
