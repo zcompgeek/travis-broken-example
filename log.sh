a17d39d1180c8b32c0ab814ee6ab6ad27b158a0a
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 12:31:21 -0400

    backtick

--- log.sh
+++ log.sh
@@ -4,7 +4,7 @@ set -ev
 STR="Hello World!"
 echo $STR
 
-LG=git log --oneline $TRAVIS_COMMIT_RANGE
+LG= `git log --oneline $TRAVIS_COMMIT_RANGE`
 echo $LG
 LINK="<https://github.com/zcompgeek/travis-broken-example/compare/$LG | Full Changelog>"
 echo $LINK
