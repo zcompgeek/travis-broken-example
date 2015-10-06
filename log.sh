7869fdbf8ebb1ab1e0b7741dcd5a3bf324670c7a
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 12:47:15 -0400

    correct link

--- log.sh
+++ log.sh
@@ -6,7 +6,7 @@ echo $STR
 
 LG=$(git log --oneline $TRAVIS_COMMIT_RANGE)
 echo $LG
-LINK="<https://github.com/zcompgeek/travis-broken-example/compare/$LG | Full Changelog>"
+LINK="<a href=\"https://github.com/zcompgeek/travis-broken-example/compare/$TRAVIS_COMMIT_RANGE\"> Full Commit Log</a>"
 echo $LINK
 
 phpunit Test.php
