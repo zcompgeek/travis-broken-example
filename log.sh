0873b768f3d255aced679b018087a65296fea9bb
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 15:35:01 -0400

    test

--- log.sh
+++ log.sh
@@ -5,7 +5,7 @@ LG=$(git log --oneline $TRAVIS_COMMIT_RANGE)
 LINK="<a href=\"https://github.com/zcompgeek/travis-broken-example/compare/$TRAVIS_COMMIT_RANGE\"> Full Commit Log</a>"
 echo $LINK
 
-if [-e ./cache/salixHash]; then
+if test -e ./cache/salixHash; then
 	OLD=$(cat ./cache/salixHash)
 	echo $OLD
 else
