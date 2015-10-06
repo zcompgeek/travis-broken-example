046a3ebe398a6fcf948a7ca30b0399c68ec32db3
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 15:18:40 -0400

    if

--- log.sh
+++ log.sh
@@ -5,9 +5,13 @@ LG=$(git log --oneline $TRAVIS_COMMIT_RANGE)
 LINK="<a href=\"https://github.com/zcompgeek/travis-broken-example/compare/$TRAVIS_COMMIT_RANGE\"> Full Commit Log</a>"
 echo $LINK
 
-echo "123dse3" > ./cache/salixHash
+if [! -e ./cache/salixHash]; then
+	mkdir ./cache
+	echo "123dse3" > ./cache/salixHash
+fi
 
 OLD=$(cat ./cache/salixHash)
+
 echo $OLD
 
 phpunit Test.php
